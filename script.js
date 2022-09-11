592e34dfe953ab7bcc753fddf8a8338a8ca9605c
Marsavin Dmitry <dimanmars@gmail.com>
2022-09-12 01:41:49 +0300

    New calendar system; 2 months now

--- script.js
+++ script.js
@@ -1,107 +1,62 @@
-function getInfoForCalendar(){
-	var newDate = new Date();
+function createCalendar(elem, year, month) {
 	
 
-	var day = newDate.getDate();
-	var month = newDate.getMonth();
-	var year = newDate.getFullYear();
-	var firstDay = new Date(year, month, 1).getDay();
-	var totalDays = new Date(year, month+1, 0).getDate();
+	let mon = month - 1; // months in JS are 0..11, not 1..12
+	let d = new Date(year, mon);
 
-	if(firstDay == 0) {
-		firstDay = 7;
+	let table = '<table><tr><th>Пн</th><th>Вт</th><th>Ср</th><th>Чт</th><th>Пт</th><th>Сб</th><th>Вс</th></tr><tr>';
+
+	// spaces for the first row
+	// from Monday till the first day of the month
+	// * * * 1  2  3  4
+	for (let i = 0; i < getDay(d); i++) {
+		table += '<td></td>';
 	}
 
-	return {
-		day: day,
-		month: month,
-		year: year,
-		firstDay: firstDay,
-		totalDays: totalDays,
-		monthName: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
-	}
-}
+	// <td> with actual dates
+	while (d.getMonth() == mon) {
+		table += '<td>' + d.getDate() + '</td>';
 
+		if (getDay(d) % 7 == 6) { // sunday, last day of week - newline
+			table += '</tr><tr>';
+		}
 
-
-function fillDates(yr,mth,dy){
-	var myDate = getInfoForCalendar();
-	
-	var active = 1;
-	var activeArr = [1];
-	var rows = 1;
-	var rowsArr = [1];
-
-	while(myDate.day - (active+7) > 0){
-		active+=7;
-		activeArr.push(active+7);
-
-		rows++;
-		rowsArr.push(rows+1);
+		d.setDate(d.getDate() + 1);
 	}
 
-	active = activeArr[activeArr.length-1];
-
-	if (active<=7){
-		active += 7;
-	}
-	rows = rowsArr[rowsArr.length-1];
-	if (rows == 1 && myDate.day >= 7-myDate.firstDay ) {
-		rows = 2;
-	}
-
-	var x = active - myDate.day;
-	var currDay = 0;
-	if(x > 0){
-		currDay = myDate.firstDay - x;
-	} else {
-		currDay = myDate.firstDay + Math.abs(x);
-	}
-
-	if(currDay<=0){
-		currDay = 7 - Math.abs(currDay);
-		if(rows>1){
-			rows = rows-1;
+	// add spaces after last days of month for the last row
+	// 29 30 31 * * * *
+	if (getDay(d) != 0) {
+		for (let i = getDay(d); i < 7; i++) {
+			table += '<td></td>';
 		}
 	}
 
-	
+	// close the table
+	table += '</tr></table>';
 
-	for (var i = 1; i <= myDate.totalDays; i++) {
+	elem.innerHTML = table;
 
-		fillDay(i <= 7 - myDate.firstDay+1, true, 1, myDate.firstDay+(i-1), rows==1, currDay , i);
-		fillDay(i > 7 - myDate.firstDay+1, i <= 14 - myDate.firstDay+1, 2, myDate.firstDay+(i-8), rows==2, currDay, i);
-		fillDay(i > 14 - myDate.firstDay+1, i <= 21 - myDate.firstDay+1, 3, myDate.firstDay+(i-15), rows==3, currDay, i);
-		fillDay(i > 21 - myDate.firstDay+1, i <= 28 - myDate.firstDay+1, 4, myDate.firstDay+(i-22), rows==4, currDay, i);
-		fillDay(i > 28 - myDate.firstDay+1, i <= 35 - myDate.firstDay+1, 5, myDate.firstDay+(i-29), rows==5, currDay, i);
-		fillDay(i > 35 - myDate.firstDay+1, true, 6, myDate.firstDay+(i-36), rows==6, currDay, i);
-	} 
+	const months = ["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"];
+	let monthName = months[d.getMonth()];
+	elem.previousElementSibling.innerText = monthName;
+}
 
-	jQuery('#date').text(`${myDate.monthName[myDate.month]}`);
+function getDay(date) { // get day number from 0 (monday) to 6 (sunday)
+	let day = date.getDay();
+	if (day == 0) day = 7; // make Sunday (0) the last day
+	return day - 1;
 }
 
 
-function fillDay(minDay, maxDay, weekNo, weekDayNo, conditionCurrentDay, currentDay, loop){
-	if(minDay && maxDay){
-		jQuery('tbody tr:nth-child('+weekNo+') td:nth-child(' + weekDayNo + ')').text(loop);
-		if( conditionCurrentDay ) {
-				jQuery('tbody tr:nth-child('+weekNo+') td:nth-child(' + currentDay + ')').addClass('current');
-			}
-	}
-}
+const date = new Date();
+
+createCalendar(calendar1, 2012, date.getMonth());
+createCalendar(calendar2, 2012, (date.getMonth() + 1));
 
 
- 
-
-(function(){
-	
-	fillDates();
-
-})();
-
-
-document.addEventListener("click", function(e) {
-  console.log(e.target);
+document.addEventListener("click", function(event) {
+  console.log(event.target);
   if (event.target.tagName.toLowerCase() === 'td') {
     event.target.classList.toggle("active");
   }
